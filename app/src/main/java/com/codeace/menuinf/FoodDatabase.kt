9c40e0d4272f06cfe32a63c2d3b0001bb35eaea0
sawal11 <t.sawal231@gmail.com>
2019-07-16 15:37:04 +0545

    Intent required

--- app/src/main/java/com/codeace/menuinf/FoodDatabase.kt
+++ app/src/main/java/com/codeace/menuinf/FoodDatabase.kt
@@ -1,11 +1,33 @@
 package com.codeace.menuinf
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
-
 
 @Database(entities = [FoodData::class], version = 1)
 abstract class FoodDatabase : RoomDatabase() {
+
     abstract fun foodDataDao(): FoodDataDao
 
+    companion object {
+
+        @Volatile
+        private var INSTANCE: FoodDatabase? = null
+
+        internal fun getDatabase(context: Context): FoodDatabase? {
+            if (INSTANCE == null) {
+                synchronized(FoodDatabase::class.java) {
+                    if (INSTANCE == null) {
+                        INSTANCE = Room.databaseBuilder(
+                            context.applicationContext,
+                            FoodDatabase::class.java, "word_database"
+                        )
+                            .build()
+                    }
+                }
+            }
+            return INSTANCE
+        }
+    }
 }
