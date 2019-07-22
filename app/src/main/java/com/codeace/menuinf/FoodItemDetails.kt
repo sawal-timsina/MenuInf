4ec33bd52da6d7e160c0dffcbe63a2f4e727ef1d
sawal11 <t.sawal231@gmail.com>
2019-07-22 10:36:08 +0545

    Shared Transaction

---
+++ app/src/main/java/com/codeace/menuinf/FoodItemDetails.kt
@@ -0,0 +1,20 @@
+package com.codeace.menuinf
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.item_details.*
+
+
+class FoodItemDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.item_details)
+
+        val foodData: FoodData = intent.extras!!.get("extra_object") as FoodData
+        FoodAdapter.setImage(this, foodData.image, foodImage)
+        itemName_.text = foodData.name
+        itemCategory_.text = foodData.category
+        itemSpiciness_.text = foodData.spiciness
+        itemPrice_.text = foodData.price.toString()
+    }
+}
